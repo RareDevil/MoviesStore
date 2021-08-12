63cb88c626eb636a3854c316bd6f55c75c4a749d
RareDevil <k_gliese@hotmail.com>
2021-08-12 17:52:35 +0200

    Created the dropdown for the langauge and location. Also made the langauge dropdown work.

--- MoviesStore/src/Main.vue
+++ MoviesStore/src/Main.vue
@@ -18,6 +18,32 @@
                         </li>
                     </ul>
                     <div class="d-flex" v-if="$route.name === 'Home'">
+                        <div class="collapse navbar-collapse" id="navbarNavLanguageDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarLanguageDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Language
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarLanguageDropdownMenuLink">
+                                        <li><span class="dropdown-item" v-on:click="changeLang('All')">All</span></li>
+                                        <li v-for="lang in languages" v-bind:key="lang"><span class="dropdown-item" v-on:click="changeLang(lang)">{{ lang }}</span></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="collapse navbar-collapse" id="navbarNavLocationDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarLocationDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Location
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarLocationDropdownMenuLink">
+                                        <li><span class="dropdown-item">All</span></li>
+                                        <li v-for="loc in locations" v-bind:key="loc"><span class="dropdown-item" >{{ loc }}</span></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                         <input class="form-control me-2 searchField" type="search" placeholder="Search" aria-label="Search" v-on:keypress="keypressedInSearch">
                         <button class="btn btn-outline-success" type="button" v-on:click="search">Search</button>
                     </div>
@@ -38,8 +64,18 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: 'Main',
+    data: () => ({
+        languages: {},
+        locations: {},
+    }),
+    created () {
+        axios.get('/api/movie/languages').then(({ data }) => this.languages = data);
+        axios.get('/api/movie/locations').then(({ data }) => this.locations = data);
+    },
     methods: {
         keypressedInSearch: function (event) {
             if (event.key === "Enter") {
@@ -54,7 +90,15 @@ export default {
                 params: {
                     query: searchfield[0].value
                 }
-            })
+            });
+        },
+        changeLang: function(lang) {
+            this.$router.push({
+                name: 'Home',
+                params: {
+                    language: (lang === "All" ? "": lang)
+                }
+            });
         }
     }
 }
--- MoviesStore/src/views/Home.vue
+++ MoviesStore/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="album py-5 home">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 moviesCollection">
 
-          <div class="col" v-for="value in moviesCollection" v-bind:key="value">
+          <div class="col" v-for="value in moviesCollection" v-bind:key="value.imdbID">
               <div class="card shadow-sm">
                   <router-link v-bind:to="/movie/ + value.imdbID">
                       <img class="bd-placeholder-img card-img-top" v-bind:src="value.poster" v-bind:alt="value.title">
@@ -22,18 +22,40 @@ import axios from 'axios';
 
 export default {
     name: 'Home',
-    props: ['query'],
+    props: ['query', 'language', 'location'],
     data: () => ({
         moviesCollection: {}
     }),
     created () {
-        return axios.get('/api/movies').then(({ data }) => this.moviesCollection = data);
+        axios.get('/api/movies').then(({ data }) => this.moviesCollection = data);
     },
     watch: {
-        query: function(newVal, oldVal) {
-            console.log('Prop changed: ', newVal, ' | was: ', oldVal)
-            return axios.get('/api/movies?query=' + newVal).then(({ data }) => this.moviesCollection = data);
+        query: function(newVal) {
+            return axios.get('/api/movies?query=' + this.getValueOrEmptyString(newVal) +
+                '&language=' + this.getValueOrEmptyString(this.language) +
+                '&location' + this.getValueOrEmptyString(this.location))
+                .then(({ data }) => this.moviesCollection = data);
+        },
+        language: function(newVal) {
+            return axios.get('/api/movies?query=' + this.getValueOrEmptyString(this.query) +
+                '&language=' + this.getValueOrEmptyString(newVal) +
+                '&location' + this.getValueOrEmptyString(this.location))
+                .then(({ data }) => this.moviesCollection = data);
+        },
+        location: function(newVal) {
+            return axios.get('/api/movies?query=' + this.getValueOrEmptyString(this.query) +
+                '&language=' + this.getValueOrEmptyString(this.language) +
+                '&location' + this.getValueOrEmptyString(newVal))
+                .then(({ data }) => this.moviesCollection = data);
         }
+    },
+    methods: {
+        getValueOrEmptyString: function (value) {
+            if (value == null) {
+                return "";
+            }
+            return value;
+        },
     }
 }
 </script>
